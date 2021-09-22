e3b336d103388f27679c61310d4fa2fa86d40d50
DevLinky <xpog.devlinky@gmail.com>
2021-09-22 21:20:10 +0200

    Update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import "../Style/Card.css"
 import Collection from './Collection';
+import cardBorder from '../images/cartes/Carte_Modele_Degats_ss_icones.png'
 
-const borderImg = '../images/cartes/Carte_Modele_Degats_ss_icones.png'
+
 
 
 const Card = ({name,type,image,mana,attack,HP,desc,effect, addToDeck}) => {
 
     return (
-        <div className="carte"  onClick={() => addToDeck(name)} style={{backgroundImage :`url(${borderImg})`}}>
-            <img className="carteBord" src={image} alt="Bord de carte"/>
+        <div className="carte"  onClick={() => addToDeck(name)} style={{backgroundImage :`url(${image})`}}>
+            <img className="carteBord" src={cardBorder} alt="Bord de carte"/>
             <div className="carteName">{name}</div>
             <div className="carteType">{type[0]}-{type[1]}</div>
             <div className="carteMana">{mana}</div>
--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,22 +1,38 @@
 import React, { useState } from 'react';
 import { test } from './tempListOfCards'
 import Card from './Card';
+import Swal from 'sweetalert2'
+import { Fetch } from './fetch';
 
 
 
 
 export default function Collection() {
     const [deck, setDeck] = useState([]);
+    const deleteDeck = () => {
+        setDeck([])
+    }
     const addToDeck = async (nameTag, mana) => {
         
+        if(deck.length===20){
+            Swal.fire({
+                icon: 'error',
+                title: 'IMPOSSIBLE',
+                text: 'Votre deck contient déjà le maximum de cartes !',
+              })
+        }else{
         await setDeck([...deck, nameTag]);
         console.log(nameTag);
         console.log(deck);
     }
+    }
 
-
+const url = 'http://localhost:5000/cards';
     return (
         <div className="Collection">
+            <button onClick={deleteDeck} >Effacer le deck</button>
+            <Fetch url={url}/>
+            
             <div className="cards" >
                 {test.map((cards) => {
                     return (
@@ -27,7 +43,7 @@ export default function Collection() {
                 })}
             </div>
 
-
+            
             <div className="currentDeck" >
             {deck.map((addedCards) => {
                     return (
