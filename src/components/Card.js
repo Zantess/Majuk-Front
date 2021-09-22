efb7a8c4efbdd0d9dc70da1f5323a39b5ebdc017
DevLinky <xpog.devlinky@gmail.com>
2021-09-23 01:49:57 +0200

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,7 +12,7 @@ const Card = ({name,type,image,mana,attack,HP,desc,effect, addToDeck}) => {
         <div className="carte"  onClick={() => addToDeck(name)} style={{backgroundImage :`url(${image})`}}>
             <img className="carteBord" src={cardBorder} alt="Bord de carte"/>
             <div className="carteName">{name}</div>
-            <div className="carteType">{type[0]}-{type[1]}</div>
+            <div className="carteType">{type[0]}{type[1]}</div>
             <div className="carteMana">{mana}</div>
             <div className="carteAttack">{attack}</div>
             <div className="carteHP">{HP}</div>
--- src/components/Collection.js
+++ src/components/Collection.js
@@ -10,25 +10,26 @@ export default function Collection() {
         setDeck([])
     }
     const addToDeck = async (nameTag, mana) => {
-        
-        if(deck.length===20){
+
+        if (deck.length === 20) {
             Swal.fire({
                 icon: 'error',
                 title: 'IMPOSSIBLE',
                 text: 'Votre deck contient déjà le maximum de cartes !',
-              })
-        }else{
-        await setDeck([...deck, nameTag]);
-        console.log(nameTag);
-        console.log(deck);
-    }
+            })
+        } else {
+            await setDeck([...deck, nameTag]);
+            console.log(nameTag);
+            console.log(deck);
+        }
     }
 
-const url = 'http://localhost:5000/cards';
+    const url = 'http://localhost:5000/cards';
+    
     return (
         <div className="Collection">
             <button onClick={deleteDeck} >Effacer le deck</button>
-            
+
             <div className="cards" >
                 {test.map((cards) => {
                     return (
@@ -39,9 +40,9 @@ const url = 'http://localhost:5000/cards';
                 })}
             </div>
 
-            
+
             <div className="currentDeck" >
-            {deck.map((addedCards) => {
+                {deck.map((addedCards) => {
                     return (
 
                         <div className="selectedCard">
